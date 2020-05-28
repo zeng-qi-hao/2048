c415002ed8ad3fb1daeabc420d8ebded4931c5bf
zeng-qi-hao <60589863+zeng-qi-hao@users.noreply.github.com>
2020-05-28 16:04:44 +0800

    Add files via upload

--- js/support2048.js
+++ js/support2048.js
@@ -1,8 +1,13 @@
+documentWidth=window.screen.availWidth;
+gridContainerWidth=0.92*documentWidth;
+cellSideLength=0.18*documentWidth;
+cellSpace=0.04*documentWidth;
+
 function getPosTop(i,j){
-	return 20 + i*120;
+	return cellSpace + i*(cellSpace+cellSideLength);
 }
 function getPosLeft(i,j){
-	return 20 + j*120;
+	return cellSpace + j*(cellSpace+cellSideLength);
 }
 
 
